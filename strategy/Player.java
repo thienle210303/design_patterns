aa2f29e3ee3d06788d40604361ad5133aa828283
Thien Le <thienle210303@gmail.com>
2024-01-09 19:47:28 -0500

    Add player

---
+++ strategy/Player.java
@@ -0,0 +1,35 @@
+import java.util.Random;
+
+public class Player {
+  // variable
+  private String firstName;
+  private String lastName;
+  protected DefenceBehavior defenceBehavior;
+  protected OffenceBehavior offenceBehavior;
+  protected Random rand;
+
+  // constructor
+  public Player(String firstName, String lastName, boolean offenceBehavior) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  // mutator
+  public void setDefenceBehavior() {
+    this.setDefenceBehavior != this.setDefenceBehavior;
+  }
+
+  public void setOffenceBehavior() {
+    this.setOffenceBehavior != this.setOffenceBehavior;
+
+  }
+
+  // method
+  public String play(boolean possession) {
+    return "";
+  }
+
+  public String toString() {
+    return "";
+  }
+}
