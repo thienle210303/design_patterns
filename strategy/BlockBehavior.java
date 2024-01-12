de9b8459564bff566c2cace65c2066952bd36469
Thien Le <thienle210303@gmail.com>
2024-01-11 23:46:34 -0500

    new

--- strategy/BlockBehavior.java
+++ strategy/BlockBehavior.java
@@ -5,7 +5,6 @@ public class BlockBehavior implements DefenseBehavior {
   public String play() {
     Random rand = new Random();
     int randNum = rand.nextInt(3) + 1;
-    // int randNum = super.rand.nextInt(3) + 1;
 
     if (randNum == 1) {
       return "blocks player from passing";
--- strategy/BlockGoalBehavior.java
+++ strategy/BlockGoalBehavior.java
@@ -3,11 +3,15 @@ import java.util.Random;
 
 public class BlockGoalBehavior implements OffenceBehavior, DefenseBehavior {
   public String play() {
-    // Random rand = new Random();
-    // int randNum = rand.nextInt(2) + 1;
-    // if(randNum == 1) {
-    //   return 
-    // }
-    return "block goal behavior";
+    Random rand = new Random();
+    int randNum = rand.nextInt(4) + 1;
+    if(randNum == 1) {
+      return "hand blocks the puck";
+    } else if(randNum == 2) {
+      return "catches the puck";
+    } else if(randNum == 3) {
+      return "blocks puck with knee pads";
+    }
+    return "blocks puck with stick";
   }
 }
